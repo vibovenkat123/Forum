497fffbad89d9e1ed01e52b0b86b8b2caa8530ae
vibovenkat123 <vibovenkat@gmail.com>
2022-10-29 12:13:50 -0500

    add routing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<div>
+  <header>
+    <div>
+      <a href="/">Home</a>
+      <a href="/New">Add</a>
+      <a href="/View">View</a>
+    </div>
+  </header>
+</div>
+
+<slot />
---
+++ src/routes/View/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let data: {
+    all: {
+      comments: {
+        author: string;
+        body: string;
+        id: number;
+        title: string;
+      }[];
+    };
+  };
+</script>
+
+<main>
+  <div>
+    {#each data.all.comments as comment}
+      <div class="comment">
+        <h2>{comment.title}</h2>
+        <p>{comment.body}</p>
+        <h4>{comment.author}</h4>
+      </div>
+    {/each}
+  </div>
+</main>
+
+<style>
+  .comment {
+    border: 2px solid black;
+    border-radius: 2em;
+    padding: 0.5em;
+    margin-top: 1em;
+  }
+</style>
